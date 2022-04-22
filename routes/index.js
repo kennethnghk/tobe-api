32f54308f01ac76e4847ab21688437af2d481989
Kenneth Ng <kenneth.ng@km-it.cn>
2022-04-22 17:49:22 +0800

    Add dotenv

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Express', publicKey: process.env.MONGO_ATLAS_PUBLIC_KEY })
 })
 
 module.exports = router
