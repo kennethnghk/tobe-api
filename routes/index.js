eb07460e413d4a1a9931abbc6d37d3c6f1741992
Kenneth Ng <kenneth.ng@km-it.cn>
2022-04-22 18:20:32 +0800

    Add mongoose

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var router = express.Router()
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', publicKey: process.env.MONGO_ATLAS_PUBLIC_KEY })
+
+  const Cat = mongoose.model('Cat', { name: String })
+
+const kitty = new Cat({ name: 'Zildjian2' })
+kitty.save().then(() => console.log('meow'))
+
+  res.render('index', { title: 'Express' })
 })
 
 module.exports = router
